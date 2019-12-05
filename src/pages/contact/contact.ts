3106be89328688c39d63cc61ec6cf1945cac3c6a
Aldo <aldoe.profuturo@gmail.com>
2019-12-05 17:51:43 -0600

    primer commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -15,11 +15,42 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: 'contact.html',
 })
 export class ContactPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  nombre = "";
+  telefono = "";
+  correo_electronico = "";
+  facebook = "";
+  twitter = "";
+  instagram = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public http: HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
+  }
+
+  addContact() {
+    console.log('addContact');
+
+    const contact = {
+      nombre: this.nombre,
+      telefono: this.telefono,
+      correo_electronico: this.correo_electronico,
+      facebook: this.facebook,
+      twitter: this.twitter,
+      instagram: this.instagram
+    };
+    console.log(JSON.stringify(contact));
+
+    this.http.post('/', contact)
+      .subscribe(data => {
+        console.log(JSON.stringify(data));
+        this.navCtrl.pop();
+      }, error => {
+        console.log(JSON.stringify(error));
+        this.navCtrl.pop();
+      });
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {HttpClient} from "@angular/common/http";
-import {ContactPage} from '../contact/contact'
+import {ContactPage} from '../contact/contact';
 
 @Component({
   selector: 'page-home',
@@ -11,18 +11,24 @@ export class HomePage {
   contactos: any;
   paginaContacto = ContactPage;
   constructor(public navCtrl: NavController, public http: HttpClient ) {
-    this.http.get('/Contactos')
-    .subscribe(data => {
-       this.contactos = data;
-       console.log(JSON.stringify(data));
-     }, error => {
-       console.log('fallo la conexion');
-     });
+    
   }
 
   ionViewWillEnter()
   {
+    this.http.get('/Contactos/Contactos')
+    .subscribe(data => {
+       this.contactos = data;
+       console.log(JSON.stringify(data));
+     }, error => {
+       console.log(JSON.stringify(error));
+     });
     
+  }
+
+  clickAdd(){
+    console.log('clickAdd');
+    this.navCtrl.push(this.paginaContacto);
   }
 
 }
